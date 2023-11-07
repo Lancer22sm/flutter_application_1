1ba1d319a8e580842c02ad8390acd313961afa97
Lancer22sm <daniil.egorov.96@mail.ru>
2023-11-07 11:46:17 +0300

    Разместил по разным файлам

---
+++ lib/my_methods.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+
+class MyMethods extends StatelessWidget {
+  const MyMethods({super.key});
+
+  Widget FirstOrSecondScreen(bool first, String namePeople) {
+    if (first) {
+      return Text(
+        namePeople,
+        textAlign: TextAlign.left,
+        style: const TextStyle(color: Colors.black),
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
+        softWrap: false,
+      );
+    } else {
+      return const TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(bottom: 10),
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            hintText: 'Введите текст'),
+      );
+    }
+  }
+
+  Column myColumnStatePeople(
+      String namePeople,
+      bool state,
+      double containerWidth,
+      Color containerColor,
+      String statePeople,
+      double containerPadding,
+      bool first) {
+    if (state) {
+      return Column(
+        children: <Widget>[
+          Container(
+              alignment: Alignment.bottomLeft,
+              padding: const EdgeInsets.only(left: 5),
+              width: 180,
+              height: 20,
+              child: FirstOrSecondScreen(first, namePeople)),
+          Padding(
+            padding: EdgeInsets.only(right: containerPadding, left: 5, top: 5),
+            child: Container(
+              alignment: Alignment.center,
+              width: containerWidth,
+              height: 20,
+              decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: const BorderRadius.all(Radius.circular(15)),
+                  color: containerColor),
+              child: Text(
+                statePeople,
+                maxLines: 1,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(left: 5),
+              width: 180,
+              height: 20,
+              child: FirstOrSecondScreen(first, namePeople))
+        ],
+      );
+    }
+  }
+
+  Row myRowPeople(
+      String namePeople,
+      Image imagePeople,
+      Icon iconMicro,
+      Icon iconCamera,
+      Icon iconHand,
+      bool state,
+      double containerWidth,
+      Color containerColor,
+      String statePeople,
+      double containerPadding,
+      double heightSizeBox,
+      bool first) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Column>[
+          myColumnPeople(
+              namePeople,
+              imagePeople,
+              iconMicro,
+              iconCamera,
+              iconHand,
+              state,
+              containerWidth,
+              containerColor,
+              statePeople,
+              containerPadding,
+              heightSizeBox,
+              first)
+        ]);
+  }
+
+  Row myRowStroke() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Column>[myColumnStroke()],
+    );
+  }
+
+  Column myColumnPeople(
+      String namePeople,
+      Image imagePeople,
+      Icon iconMicro,
+      Icon iconCamera,
+      Icon iconHand,
+      bool state,
+      double containerWidth,
+      Color containerColor,
+      String statePeople,
+      double containerPadding,
+      double heightSizeBox,
+      bool first) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        const Padding(padding: EdgeInsets.only(top: 10)),
+        Row(
+          children: <Widget>[
+            const Padding(padding: EdgeInsets.only(left: 5, right: 10)),
+            ClipOval(
+              child: SizedBox.fromSize(
+                size: const Size.fromRadius(12), // Image radius
+                child: imagePeople,
+              ),
+            ),
+            SizedBox(
+              width: 320,
+              height: heightSizeBox,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    myColumnStatePeople(namePeople, state, containerWidth,
+                        containerColor, statePeople, containerPadding, first),
+                    SizedBox(
+                        width: 130,
+                        child: Wrap(
+                          spacing: 5,
+                          children: <Widget>[
+                            iconHand,
+                            iconMicro,
+                            iconCamera,
+                            const Padding(padding: EdgeInsets.only(left: 5)),
+                            const Icon(Icons.arrow_drop_down,
+                                color: Colors.black)
+                          ],
+                        ))
+                  ]),
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  Column myColumnStroke() {
+    return Column(
+      children: <Widget>[
+        InkWell(
+          child: Container(
+              color: Colors.grey,
+              margin: const EdgeInsets.only(right: 10, left: 10, top: 10),
+              width: 340,
+              height: 1),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('dsds'),
+      ),
+    );
+  }
+}
--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,118 +1,21 @@
+
 import 'package:flutter/material.dart';
-import 'second_screen.dart';
 import 'package:flutter_application_1/my_methods.dart';
 
-
 var _MyMethods = const MyMethods();
-// Вход в приложение
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-// Маршрутизация домашней страницы приложения
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String dropdownvalue = 'Все';
-  var items = [
-    'Все',
-    'Онлайн',
-    'Оффлайн',
-  ];
-
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key, Key});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Column>[
-                Column(
-                  children: <Widget>[
-                    const Padding(padding: EdgeInsets.only(top: 40)),
-                    Container(
-                      margin: const EdgeInsets.only(left: 10, right: 5),
-                      height: 40,
-                      width: 290,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(5)),
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1,
-                          )),
-                      child: DropdownButton(
-                        menuMaxHeight: 200,
-                        isExpanded: true,
-                        value: dropdownvalue,
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                        ),
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        items: items.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownvalue = newValue!;
-                          });
-                        },
-                      ),
-                    )
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    const Padding(padding: EdgeInsets.only(top: 40)),
-                    InkWell(
-                      child: Container(
-                        margin: const EdgeInsets.only(right: 10, left: 5),
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(5)),
-                            border: Border.all(
-                              color: Colors.lightBlue,
-                              width: 1,
-                            )),
-                        child: const Icon(Icons.add, color: Colors.lightBlue),
-                      ),
-                      onTap: () {},
-                    ),
-                  ],
-                )
-              ],
-            ),
-            const Padding(padding: EdgeInsets.only(top: 30)),
-            _MyMethods.myRowPeople(
+      appBar: AppBar(
+        title: const Text("haha"),
+      ),
+      body: Column(
+        children:<Widget>[
+          _MyMethods.myRowPeople(
                 "Александо Толстиков",
                 Image.asset('assets/images/image1.jpg', width: 24, height: 24),
                 const Icon(Icons.mic_off, color: Colors.blueGrey),
@@ -131,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 "Отошёл",
                 115,
                 20,
-                true),
+                false),
             _MyMethods.myRowStroke(),
             _MyMethods.myRowPeople(
                 "Ассистентовый Вадим",
@@ -149,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 "Отошёл",
                 115,
                 45,
-                true),
+                false),
             _MyMethods.myRowStroke(),
             _MyMethods.myRowPeople(
                 "Поинтов Пётр Петрович",
@@ -167,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 "Отошёл",
                 115,
                 20,
-                true),
+                false),
             _MyMethods.myRowStroke(),
             _MyMethods.myRowPeople(
                 "Селекторный Кирил Валерьевич",
@@ -185,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 "Отошёл",
                 115,
                 20,
-                true),
+                false),
             _MyMethods.myRowStroke(),
             _MyMethods.myRowPeople(
                 "Худенков Александр",
@@ -203,9 +106,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 "Отошёл",
                 115,
                 45,
-                true),
+                false),
             _MyMethods.myRowStroke(),
-           _MyMethods.myRowPeople(
+            _MyMethods.myRowPeople(
                 "Цапля Андрей Андреевич",
                 Image.asset('assets/images/image6.jpg', width: 24, height: 24),
                 const Icon(Icons.mic_off, color: Colors.blueGrey),
@@ -221,7 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 "Контакт оффлайн",
                 55,
                 45,
-                true),
+                false),
             _MyMethods.myRowStroke(),
             _MyMethods.myRowPeople(
                 "Черный Артём Генадьевич",
@@ -239,14 +142,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 "Звоним...",
                 110,
                 45,
-                true),
-                const Padding(padding: EdgeInsets.only(top: 50)),
-                ElevatedButton(onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondScreen()),);
-                }, child: const Text("Переход на другую страницу"))
-          ],
+                false)
+        ]
         ),
-      ),
     );
   }
 }
