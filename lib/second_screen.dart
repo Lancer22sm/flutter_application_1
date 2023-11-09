d0a3e7cc85371ec54559c03793c1c243ee484565
Lancer22sm <daniil.egorov.96@mail.ru>
2023-11-09 14:26:19 +0300

    ещё попытка

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -28,37 +28,7 @@ Column myColumns = Column(children: <Widget>[
             20,
             false),
         const Padding(padding: EdgeInsets.only(top: 40)),
-        GestureDetector(
-            onTap: () {
-              var containerss = Container(
-                width: 40,
-                height: 20,
-                color: Colors.green,
-              );
-              var myRowStroke = _MyMethods.myRowStroke();
-              var myRowPeople = _MyMethods.myRowPeople(
-                  _MyMethods.getTextFromField(),
-                  Image.asset('assets/images/image1.jpg',
-                      width: 24, height: 24),
-                  const Icon(Icons.mic_off, color: Colors.blueGrey),
-                  const Icon(
-                    Icons.videocam_off,
-                    color: Colors.blueGrey,
-                  ),
-                  const Icon(
-                    Icons.back_hand,
-                    color: Colors.white,
-                    size: 20,
-                  ),
-                  false,
-                  65,
-                  Colors.grey,
-                  "Отошёл",
-                  115,
-                  20,
-                  false);
-            },
-            child: ElevatedButton(
+        ElevatedButton(
                 onPressed: () {
                   myColumns.children.add(_MyMethods.myRowStroke());
                   myColumns.children.add(_MyMethods.myRowPeople(
@@ -83,7 +53,7 @@ Column myColumns = Column(children: <Widget>[
                   20,
                   false));
                 },
-                child: const Text("Переход на другую страницу")))
+                child: const Text("Переход на другую страницу"))
       ]);
 
 class SecondScreen extends StatelessWidget {
