ab635c28950dd0d671e2934ae6c88a4b730bcff4
Lancer22sm <daniil.egorov.96@mail.ru>
2023-11-09 14:24:27 +0300

    есть попытка

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/my_methods.dart';
 import 'dart:ui';
+import 'dart:async';
+import 'package:flutter/services.dart';
 
 var _MyMethods = MyMethods();
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key, Key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Вторая страница"),
-      ),
-      body: Column(children: <Widget>[
+Column myColumns = Column(children: <Widget>[
         _MyMethods.myRowPeople(
             "Александо Толстиков",
             Image.asset('assets/images/image1.jpg', width: 24, height: 24),
@@ -38,8 +31,8 @@ class SecondScreen extends StatelessWidget {
         GestureDetector(
             onTap: () {
               var containerss = Container(
-                width: 120,
-                height: 2,
+                width: 40,
+                height: 20,
                 color: Colors.green,
               );
               var myRowStroke = _MyMethods.myRowStroke();
@@ -64,27 +57,45 @@ class SecondScreen extends StatelessWidget {
                   115,
                   20,
                   false);
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => containerss,
-                ),
-              );
-              WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-                // Создаем новый виджет
-                final widget = const Text('Hello, World!');
-
-                // Добавляем виджет в дерево виджетов
-                context.replaceWith(widget);
-              });
             },
-            child: Container(
-              alignment: Alignment.center,
-              width: 180,
-              height: 20,
-              color: Colors.blue,
-              child: const Text("добавить пользователя"),
-            ))
-      ]),
+            child: ElevatedButton(
+                onPressed: () {
+                  myColumns.children.add(_MyMethods.myRowStroke());
+                  myColumns.children.add(_MyMethods.myRowPeople(
+                  _MyMethods.getTextFromField(),
+                  Image.asset('assets/images/image1.jpg',
+                      width: 24, height: 24),
+                  const Icon(Icons.mic_off, color: Colors.blueGrey),
+                  const Icon(
+                    Icons.videocam_off,
+                    color: Colors.blueGrey,
+                  ),
+                  const Icon(
+                    Icons.back_hand,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  false,
+                  65,
+                  Colors.grey,
+                  "Отошёл",
+                  115,
+                  20,
+                  false));
+                },
+                child: const Text("Переход на другую страницу")))
+      ]);
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key, Key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Вторая страница"),
+      ),
+      body: myColumns
     );
   }
 }
