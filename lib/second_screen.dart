f9425774645dd2dafd80a8d58fcd8b02642960cb
Lancer22sm <daniil.egorov.96@mail.ru>
2023-11-09 14:59:27 +0300

    красный остался класс

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/my_methods.dart';
-import 'dart:ui';
-import 'dart:async';
-import 'package:flutter/services.dart';
 
 var _MyMethods = MyMethods();
 
@@ -33,6 +30,12 @@ class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key, Key});
 
   @override
+  _SecondScreen createState() => _SecondScreen();
+}
+
+class _SecondScreen extends State<SecondScreen> {
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -44,8 +47,9 @@ class SecondScreen extends StatelessWidget {
         const Padding(padding: EdgeInsets.only(top: 40)),
         ElevatedButton(
                 onPressed: () {
-                  myColumns.children.add(_MyMethods.myRowStroke());
-                  myColumns.children.add(_MyMethods.myRowPeople(
+                  setState(() {
+                    myColumns.children.add(_MyMethods.myRowStroke());
+                    myColumns.children.add(_MyMethods.myRowPeople(
                   _MyMethods.getTextFromField(),
                   Image.asset('assets/images/image1.jpg',
                       width: 24, height: 24),
@@ -66,6 +70,7 @@ class SecondScreen extends StatelessWidget {
                   115,
                   20,
                   true));
+                  });
                 },
                 child: const Text("Переход на другую страницу"))
         ]
