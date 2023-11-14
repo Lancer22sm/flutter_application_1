a4ba2737387bac87066dbc055fe55bea8b6e1c00
Lancer22sm <daniil.egorov.96@mail.ru>
2023-11-14 11:47:38 +0300

    Json работает

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_application_1/my_methods.dart';
+import 'package:path_provider/path_provider.dart';
 
 var _MyMethods = MyMethods();
 
@@ -40,8 +40,6 @@ class SecondScreen extends StatefulWidget {
 class _SecondScreen extends State<SecondScreen> {
   final _items = <Widget>[];
 
-  
-
   void _addItem() {
     setState(() {
       _items.add(_MyMethods.myRowStroke());
@@ -65,19 +63,63 @@ class _SecondScreen extends State<SecondScreen> {
           115,
           20,
           true));
-          jsonCollect();
+      jsonCollect();
     });
   }
 
+  localFile(path) async {
+    return File('$path/MyNames.json');
+  }
+
   void jsonCollect() async {
-    final String asset = 'assets/jsons/mynames.json';
-    File modifiedFile = File(asset);
+    List<String> myNames = [];
     //final file = File(await DefaultAssetBundle.of(context).loadString('assets/jsons/mynames.json'));
     var jsonData = await rootBundle.loadString('assets/jsons/mynames.json');
     Map<String, dynamic> data = jsonDecode(jsonData) as Map<String, dynamic>;
     data["names"].add(_MyMethods.getTextFromField());
+    writeJSON(data);
+    Map<String, dynamic> myData = await readJSON();
+    List<dynamic> items = myData['names'];
+    myNames = items.map((dynamic element) {
+      return element.toString();
+    }).toList();
+    print(myNames);
     //file.writeAsStringSync(json.encode(data));
-    modifiedFile.writeAsStringSync(json.encode(data));
+    //modifiedFile.writeAsStringSync(json.encode(data));
+  }
+
+  localPath() async {
+    try {
+      var tempDir = await getTemporaryDirectory();
+      String tempPath = tempDir.path;
+
+      var appDocDir = await getApplicationDocumentsDirectory();
+      String appDocPath = appDocDir.path;
+      return appDocPath;
+    } catch (err) {
+      print(err);
+    }
+  }
+
+// читаем данные json
+  readJSON() async {
+    try {
+      final file = await localFile(await localPath());
+      String str = await file.readAsString();
+      return json.decode(str);
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  // записываем данные json
+  writeJSON(Map<String, dynamic> obj) async {
+    try {
+      final file = await localFile(await localPath());
+      return file.writeAsString(json.encode(obj));
+    } catch (err) {
+      print(err);
+    }
   }
 
   @override
